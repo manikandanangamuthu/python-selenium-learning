4ca9e1e926089d8ed04e3a9855e602b4d3d0357c
manikandan angamuthu <manikandana998@gmail.com>
2025-09-02 16:02:07 +0530

    Here i added comments.py file

---
+++ python_selenium_learning_package/Day1_Python_02SEP2025/comments.py
@@ -0,0 +1,12 @@
+
+# this is single line comment.
+# This single line comment will not execute while running this file
+
+
+'''Multi line comment
+This line will not executable
+This line will not executable
+This line will not executable
+# '''
+
+print("I am working with python")
