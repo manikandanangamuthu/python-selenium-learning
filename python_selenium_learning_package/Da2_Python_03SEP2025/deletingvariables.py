e2677a5cf3a4b171f18d64abe45e79d6d5bb5ba7
manikandanangamuthu <manikandana998@gmail.com>
2025-09-03 16:24:28 +0530

    Today I added datatype, keywords and variables files

---
+++ python_selenium_learning_package/Da2_Python_03SEP2025/deletingvariables.py
@@ -0,0 +1,7 @@
+a=100
+b=100
+print(a,b)
+
+del a
+print(a)
+print(b)
---
+++ python_selenium_learning_package/Operators.py
@@ -0,0 +1,16 @@
+
+# Arithemetic Operators
+a=5
+b=2
+print(a+b)
+
+print(a+b) #addition
+print(a-b)  #substation
+print(a*b)  #Multiplication
+
+print(a/b) #2.5
+print(a//b) #2 quotient
+
+print(a%b) # 1 reminder
+
+print(a**b) # 5**2 =25
